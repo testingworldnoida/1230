def4bf484e63e74ece9b613db8a7aa3c978157d9
testingworldnoida <testingworldnoida@gmail.com>
2018-06-16 12:36:32 +0530

    Hello

---
+++ Automation/src/login/A.java
@@ -0,0 +1,26 @@
+package login;
+
+
+public class A {
+
+	static int a=100;
+	
+	public A()
+	{
+		System.out.println("Cons...");
+	}
+	
+	static
+	{
+		System.out.println("Static Block");
+	}
+	
+	public static void welcome()
+	{
+		System.out.println("Welcome User...");
+	}
+	
+	
+	
+	
+}
---
+++ Automation/src/login/B.java
@@ -0,0 +1,21 @@
+package login;
+
+import java.util.Scanner;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class B {
+
+	static int a=100;
+	
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Please enter data:- ");
+		String s1 = s.nextLine();
+		System.out.println(s1);
+	}
+	
+}
